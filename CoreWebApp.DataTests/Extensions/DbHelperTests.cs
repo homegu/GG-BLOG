d3c96e9a15c8c7ec1edb0057ee156a4ae98ef00c
Administrator <Administrator@GG-PC>
2018-11-24 14:02:48 +0800

    提交测试

---
+++ CoreWebApp.DataTests/Extensions/DbHelperTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CoreWebApp.Data.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreWebApp.Data.Extensions.Tests
+{
+    [TestClass()]
+    public class DbHelperTests
+    {
+        [TestMethod()]
+        public void ExecuteSqlCommandTest()
+        {
+            using (var db = new DataContext())
+            {
+                //User user = new User();
+                //user.UserName = "gg";
+                //user.PassWord = "gg123";
+                //db.User.Add(user);
+
+                //User user1 = new User();
+                //user1.UserName = "请问请问";
+                //user1.PassWord = "gucheng123123";
+                //db.User.Add(user1);
+
+                db.SaveChanges();
+
+                var userList = db.User.ToList();
+            }
+            //DbHelper.ExecuteSqlCommand();
+            Assert.Fail();
+        }
+    }
+}
